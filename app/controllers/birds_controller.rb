43a92212bf6a152db8bfc296f765255e5296ce96
Thomas Pope <tpope0928@gmail.com>
2020-04-10 23:06:59 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,6 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }.to_json
+    birds = Bird.all
+    render json: birds
   end
 end
